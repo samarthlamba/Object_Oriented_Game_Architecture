fd2fadf3c117c932457131f36aaed63adf81db63
Matt Bowman <mab181@duke.edu>
2020-10-28 15:41:47 -0400

    Added overloaded makeGameObject for more general use of GameObjectFactory

--- src/ooga/loader/GameObjectFactory.java
+++ src/ooga/loader/GameObjectFactory.java
@@ -6,20 +6,27 @@ import java.util.ResourceBundle;
 public class GameObjectFactory<T> {
   private ResourceBundle gameBundle;
   private static final String BUNDLE_LOCATION = "%sClasses";
+  private final int width;
+  private final int height;
 
   public GameObjectFactory (String game) {
     this.gameBundle = ResourceBundle.getBundle(String.format(BUNDLE_LOCATION,game));
+    width = Integer.parseInt(gameBundle.getString("width"));
+    height = Integer.parseInt(gameBundle.getString("height"));
   }
 
-  public <T> T makeGameObject(String every, int rowNumber, int columnNumber) {
-    int width = Integer.parseInt(gameBundle.getString("width"));
-    int height = Integer.parseInt(gameBundle.getString("height"));
-    try {
+  public T makeGameObject(String symbolToConvert, int rowNumber, int columnNumber) {
+    return makeGameObject(symbolToConvert,(double)columnNumber*width, (double)rowNumber*height);
+  }
+
+  public T makeGameObject(String every, double x, double y) {
+    try{
       Class c = Class.forName(gameBundle.getString(every));
       Constructor constr = c.getDeclaredConstructor(int.class, int.class, double.class, double.class);
-      return (T) constr.newInstance(width,height,width*columnNumber,height*rowNumber);
+      return (T) constr.newInstance(width,height,x,y);
     } catch (Exception e) {
       throw new FactoryException(String.format("Symbol %s not present in this game",every),e);
     }
   }
+
 }
