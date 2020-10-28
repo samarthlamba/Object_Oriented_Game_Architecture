b31c0bcc02f23769798b52356edc24f0c797dcc7
Roshni Penmatcha <rkp13@duke.edu>
2020-10-28 12:52:45 -0400

    fixed class imports

--- src/ooga/engine/Games/Game.java
+++ src/ooga/engine/Games/Game.java
@@ -1,7 +1,17 @@
 package ooga.engine.Games;
 
-import ooga.engine.GamePlay;
+import ooga.engine.entities.Entity;
+import ooga.engine.obstacles.Obstacle;
+
+import java.util.Collection;
 
 public abstract class Game implements GamePlay {
+    public Collection<Obstacle> getBackground(){
+        return null;
+    }
+
+    public Collection<Entity> getEntities(){
+        return null;
+    }
 
 }
---
+++ src/ooga/engine/Games/Mario.java
@@ -0,0 +1,4 @@
+package ooga.engine.Games;
+
+public class Mario {
+}
