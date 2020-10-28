498bce5aa6c059abfa84d44bf61819c2dbc2944f
Roshni Penmatcha <rkp13@duke.edu>
2020-10-28 18:05:34 -0400

    added jump, need to figure out time componenet

--- src/ooga/engine/Games/Game.java
+++ src/ooga/engine/Games/Game.java
@@ -12,17 +12,28 @@ public abstract class Game implements GamePlay {
     private static final double GRAVITY = 9.8;
     public static final double HALF = .5;
     public static final double NO_INITIAL_VELOCITY = 0;
+    public static final double JUMP_TO_SCREEN_HEIGHT_RATIO = .3;
     private Bounds dimensions;
     private Collection<Obstacle> obstacles;
     private Collection<Entity> entities;
     private Collection<Obstacle> currentObstacles;
     private double timeElapsed;
+    private double jumpInitialVelocity;
+
+
 
     public Game(int screenWidth, int screenHeight, Collection<Obstacle> obstacles, Collection<Entity> entities, double timeElapsed){
         this.dimensions = new BoundingBox(0, 0, screenWidth, screenHeight);
         this.obstacles = obstacles;
         this.entities = entities;
         this.timeElapsed = timeElapsed;
+        this.currentObstacles = obstacles;
+        jumpInitialVelocity = calculateJumpVelocity(screenHeight);
+    }
+
+    private double calculateJumpVelocity(int screenHeight){
+        double jumpMaxHeight = screenHeight * JUMP_TO_SCREEN_HEIGHT_RATIO;
+        return Math.sqrt((jumpMaxHeight * GRAVITY) / HALF);
     }
 
     public Collection<Obstacle> getBackground(){
@@ -47,16 +58,14 @@ public abstract class Game implements GamePlay {
     }
 
     public Collection<Entity> getEntities(){
+        updateEntity();
         return entities;
     }
 
     private void updateEntity(){
         for(Entity entity : entities) {
             for (Obstacle obstacle : currentObstacles) {
-                if(!obstacle.hasCollided(entity)){
-                   //determine if entity collides with top of obstacle or side
-                    applyGravity(entity);
-                }
+               applyGravity(entity, obstacle, NO_INITIAL_VELOCITY);
             }
         }
     }
@@ -65,10 +74,16 @@ public abstract class Game implements GamePlay {
         return height + initialVelocity * timeElapsed + HALF * GRAVITY * timeElapsed * timeElapsed;
     }
 
-    private void applyGravity(Entity entity){
-        entity.setY(gravity(NO_INITIAL_VELOCITY, timeElapsed, entity.getY()));
+    private void applyGravity(Entity entity, Obstacle obstacle, double initialVelocity){
+        if(!obstacle.hasCollided(entity)){
+            //determine if entity collides with top of obstacle
+            entity.setY(gravity(initialVelocity, timeElapsed, entity.getY()));
+        }
     }
 
+    private void applyJump(Entity entity, Obstacle obstacle){
+        applyGravity(entity, obstacle, jumpInitialVelocity);
+    }
 
 
 }
