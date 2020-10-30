7ebdd18180a68b1f09e5a85880870e033254b098
Samarth Lamba <samarth.lamba@duke.edu>
2020-10-30 00:23:25 -0400

    created entity items:

--- src/ooga/engine/entities/Entity.java
+++ src/ooga/engine/entities/Entity.java
@@ -1,12 +1,43 @@
 package ooga.engine.entities;
 
+import javafx.scene.Node;
+
 public abstract class Entity implements Moveables {
   private final int SCENE_WIDTH;
   private final int SCENE_HEIGHT;
+  private double currentX;
+  private double currentY;
+  private int currentHitpoints;
   public Entity(int sceneWidth,int sceneHeight,  double initialX, double initialY) {
     this.SCENE_WIDTH = sceneWidth;
     this.SCENE_HEIGHT = sceneHeight;
     this.setX(initialX);
     this.setX(initialY);
   }
+  public abstract int getID();
+  public double getX(){
+    return this.currentX;
+  }
+
+  public double getY(){
+    return this.currentY;
+  }
+
+  public void setX(double inputX){
+    currentX= inputX;
+  }
+
+  public void setY(double inputY){
+    currentY=inputY;
+  }
+
+  public void setHitpoints(int hitpoints){
+    currentHitpoints=hitpoints;
+  }
+
+  public int getHitpoints(){
+    return currentHitpoints;
+  }
+
+  //add id.
 }
--- src/ooga/engine/entities/Player.java
+++ src/ooga/engine/entities/Player.java
@@ -2,9 +2,12 @@ package ooga.engine.entities;
 
 public abstract class Player extends Entity{
 
-
+  private static final int ID = 0;
   public Player(int sceneWidth, int sceneHeight, double initialX, double initialY) {
     super(sceneWidth, sceneHeight, initialX, initialY);
+  }
+  public int getID(){
+    return this.ID;
   }
 
 }
