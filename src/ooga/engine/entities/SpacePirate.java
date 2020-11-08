ccb1fad74048b5e39425a9502f739d2c245507ea
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-08 16:13:44 -0500

    space pirate small edit

--- src/ooga/engine/entities/SpacePirate.java
+++ src/ooga/engine/entities/SpacePirate.java
@@ -4,9 +4,10 @@ package ooga.engine.entities;
  * Metroid basic enemy
  */
 public class SpacePirate extends Enemy{
-
+  private static final int FULL_HEALTH = 100;
   public SpacePirate(int objectWidth, int objectHeight, double initialX, double initialY) {
     super(objectWidth, objectHeight, initialX, initialY);
+    setHitpoints(FULL_HEALTH);
   }
 
 
