b931598371728e135e4829accf78c8cf4b9a12d5
Roshni Penmatcha <rkp13@duke.edu>
2020-11-09 14:40:44 -0500

    adjusted turtle

--- src/ooga/engine/entities/object/Turtle.java
+++ src/ooga/engine/entities/object/Turtle.java
@@ -6,7 +6,7 @@ import ooga.engine.entities.enemy.Enemy;
 /**
  * gives y velocity when you jump on them
  */
-public class Turtle extends Enemy {
+public class Turtle extends Entity {
 
   public Turtle(int objectWidth, int objectHeight, double initialX, double initialY) {
     super(objectWidth, objectHeight, initialX, initialY);
