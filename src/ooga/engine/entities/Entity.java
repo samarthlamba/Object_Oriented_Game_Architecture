f6a4f2f24be891af13d249a341cc22bc416b7d39
Roshni Penmatcha <rkp13@duke.edu>
2020-11-09 22:22:27 -0500

    fixed jump height

--- src/ooga/engine/entities/Entity.java
+++ src/ooga/engine/entities/Entity.java
@@ -10,7 +10,7 @@ public abstract class Entity extends Rectangle implements Collideable, Movable {
   private int currentHitpoints = 5;
   private Node nodeObject;
   private double speed = 0;
-  private static final int JUMP_CAPACITY = -240;
+  private static final int JUMP_CAPACITY = -220;
   private double previousX;
   private double previousY;
   private double jumpCapacity = 0;
