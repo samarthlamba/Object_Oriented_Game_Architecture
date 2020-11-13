1dea975662294baff056309811daa6949aba92ea
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-12 19:32:46 -0500

    added basic outline of FSM -- need states and factory

--- src/ooga/view/FiniteStateMachineAnimation.java
+++ src/ooga/view/FiniteStateMachineAnimation.java
@@ -1,15 +1,45 @@
 package ooga.view;
 
 import ooga.engine.entities.Entity;
+import ooga.engine.games.beans.GameBean;
 
-public abstract class FiniteStateMachineAnimation {
+
+public class FiniteStateMachineAnimation {
 
     private final Entity entity;
-    public FiniteStateMachineAnimation(Entity entity){
+    private final GameBean bean;
+    private Animation jump;
+    private Animation move;
+    private Animation specialMove;
+    public FiniteStateMachineAnimation(Entity entity, GameBean bean){
         this.entity = entity;
+        this.bean = bean;
+        initialize();
+
+    }
+//property file
+    private void initialize(){
+        jump = new Animation(bean.getJumpImage(), entity.getWidth(), entity.getHeight(), bean.xOffset(), bean.yOffset(), bean.length(), bean.framePerRow(), bean.framePerCol());
+        move = new Animation(bean.getJumpImage(), entity.getWidth(), entity.getHeight(), bean.xOffset(), bean.yOffset(), bean.length(), bean.framePerRow(), bean.framePerCol());
+        specialMove = new Animation(bean.getJumpImage(), entity.getWidth(), entity.getHeight(), bean.xOffset(), bean.yOffset(), bean.length(), bean.framePerRow(), bean.framePerCol());
+
+    }
+    public void update(){
+
     }
 
-    public abstract void update();
+    public Boolean checkIfJumping(){
+
+    }
+
+    public Boolean checkIfMoving(){
+
+    }
+
+    public Boolean checkIfDoingSpecialMove(){
+
+    }
+
 
 
 
