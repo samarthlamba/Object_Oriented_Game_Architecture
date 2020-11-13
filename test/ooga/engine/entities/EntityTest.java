99714da73ddad40332230acfed0033de88154a62
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-12 22:00:05 -0500

    added entity testing

--- test/ooga/engine/entities/EntityTest.java
+++ test/ooga/engine/entities/EntityTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import javafx.scene.shape.Rectangle;
 import ooga.engine.entities.enemy.Goomba;
 import ooga.engine.entities.player.Mario;
+import ooga.engine.entities.player.Player;
+import ooga.engine.entities.player.Viking;
 import org.junit.jupiter.api.Test;
 
 class EntityTest {
   Entity testEntity = new Mario(100, 100, 50, 50);
   Entity testEntityEnemy = new Goomba(100, 100, 50, 50);
+  Entity vikingEntity = new Viking(100, 100, 10, 10);
   @Test
   void getNode() {
 
@@ -95,4 +98,28 @@ class EntityTest {
     assertEquals(false, testEntity.getFacing());
   }
 
+  @Test
+  void hasFinishedTest(){
+    assertEquals(false, testEntity.hasFinished());
+    testEntity.setFinished(true);
+    assertEquals(true, testEntity.hasFinished());
+  }
+
+  @Test
+  void leftCollideable(){
+    testEntityEnemy = new Goomba(100, 100, 50, 50);
+    assertEquals(true, testEntityEnemy.status_Alive);
+    vikingEntity.leftCollideable(testEntityEnemy);
+    assertEquals(false, testEntityEnemy.status_Alive);
+
+  }
+
+  @Test
+  void rightCollideable(){
+    testEntityEnemy = new Goomba(100, 100, 50, 50);
+    assertEquals(true, testEntityEnemy.status_Alive);
+    vikingEntity.rightCollideable(testEntityEnemy);
+    assertEquals(false, testEntityEnemy.status_Alive);
+
+  }
 }
