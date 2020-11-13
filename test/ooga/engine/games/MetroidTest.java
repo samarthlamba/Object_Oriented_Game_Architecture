2f18f3242d118001b3c35e576c8dc68eb458fef7
Matt Bowman <mab181@duke.edu>
2020-11-13 14:40:42 -0500

    Added metroid win condition

---
+++ test/ooga/engine/games/MetroidTest.java
@@ -0,0 +1,25 @@
+package ooga.engine.games;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ooga.loader.GameFactory;
+import ooga.util.DukeApplicationTest;
+import org.junit.jupiter.api.Test;
+
+public class MetroidTest extends DukeApplicationTest {
+  private final GameFactory gameFactory = new GameFactory();
+
+  @Test
+  public void testHasFinishedFalseIfEnemiesPresent() {
+    Game metroid1 = gameFactory.makeCorrectGame("MetroidLevel1");
+    assertFalse(metroid1.hasFinished());
+  }
+
+  @Test
+  public void testHasFinishedTrueIfNoEnemies() {
+    Game noEnemies = gameFactory.makeCorrectGame("testNoEnemies");
+    assertTrue(noEnemies.hasFinished());
+  }
+
+}
