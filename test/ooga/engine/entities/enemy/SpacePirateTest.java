a4c802169691fad1a5b3a0070f7d1e0e54078c68
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-12 21:50:30 -0500

    fixed status of samus issue

--- test/ooga/engine/entities/enemy/SpacePirateTest.java
+++ test/ooga/engine/entities/enemy/SpacePirateTest.java
@@ -12,9 +12,12 @@ class SpacePirateTest {
 
         Samus samus = new Samus(10, 10, 0, 0);
         SpacePirate pirate = new SpacePirate(10, 10, 0, 0);
+        int preHitLives = samus.getHitpoints();
         pirate.leftCollideable(samus);
         samus.getHitpoints();
-        assertEquals(samus.getHitpoints()-1,samus.getHitpoints());
+        assertEquals(preHitLives-1,samus.getHitpoints());
+        pirate.bottomCollideable(samus);
+        pirate.bottomCollideable(samus);
         assertEquals(false, samus.getStatusAlive());
 
     }
@@ -23,17 +26,22 @@ class SpacePirateTest {
     void rightCollideable() {
         Samus samus = new Samus(10, 10, 0, 0);
         SpacePirate pirate = new SpacePirate(10, 10, 0, 0);
+        int preHitLives = samus.getHitpoints();
         pirate.rightCollideable(samus);
-        assertEquals(samus.getHitpoints()-1,samus.getHitpoints());
-        assertEquals(false, samus.getStatusAlive());
+        assertEquals(preHitLives-1,samus.getHitpoints());
+        pirate.bottomCollideable(samus);
+        assertEquals(true, samus.getStatusAlive());
     }
 
     @Test
     void bottomCollideable() {
         Samus samus = new Samus(10, 10, 0, 0);
         SpacePirate pirate = new SpacePirate(10, 10, 0, 0);
+        int preHitLives = samus.getHitpoints();
         pirate.bottomCollideable(samus);
-        assertEquals(samus.getHitpoints()-1,samus.getHitpoints());
+        assertEquals(preHitLives-1,samus.getHitpoints());
+        pirate.bottomCollideable(samus);
+        pirate.bottomCollideable(samus);
         assertEquals(false, samus.getStatusAlive());
     }
 
@@ -41,8 +49,11 @@ class SpacePirateTest {
     void topCollideable() {
         Samus samus = new Samus(10, 10, 0, 0);
         SpacePirate pirate = new SpacePirate(10, 10, 0, 0);
+        int preHitLives = samus.getHitpoints();
         pirate.topCollideable(samus);
-        assertEquals(samus.getHitpoints()-1,samus.getHitpoints());
+        assertEquals(preHitLives-1,samus.getHitpoints());
+        pirate.bottomCollideable(samus);
+        pirate.bottomCollideable(samus);
         assertEquals(false, samus.getStatusAlive());
     }
 }
