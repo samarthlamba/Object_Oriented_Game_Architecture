6ba152905b0aaefed9351260594045321672b006
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-14 23:25:40 -0500

    throwing exception

--- src/ooga/engine/entities/MethodNotFound.java
+++ src/ooga/engine/entities/MethodNotFound.java
@@ -1,4 +1,11 @@
 package ooga.engine.entities;
 
-public class MethodNotFound {
+public class MethodNotFound  extends RuntimeException{
+    public MethodNotFound(String message) {
+        super(message);
+    }
+
+    public MethodNotFound(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/ooga/engine/entities/object/Coin.java
+++ src/ooga/engine/entities/object/Coin.java
@@ -1,6 +1,7 @@
 package ooga.engine.entities.object;
 
 import ooga.engine.entities.Entity;
+import ooga.engine.entities.MethodNotFound;
 import ooga.engine.games.GamePropertyFileReader;
 
 import java.lang.reflect.Method;
@@ -11,110 +12,104 @@ import java.util.Collection;
  */
 public class Coin extends Entity {
 
-  public static final int COIN_HEALTH = 1;
-  public static final int COIN_SIZE = 50;
+    public static final int COIN_HEALTH = 1;
+    public static final int COIN_SIZE = 50;
 
-  public Coin(int objectWidth, int objectHeight, double initialX, double initialY) {
-    super(objectWidth, objectHeight, initialX, initialY);
-    this.setHitpoints(COIN_HEALTH);
-    setWidth(COIN_SIZE);
-    setHeight(COIN_SIZE);
-    setId("coin");
-  }
-
-  @Override
-  public void leftCollideable(Entity entity) {
-    GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
-    Collection<String> methods = reader.getMethods("left");
-    System.out.println("tedsad");
-    for (String k: methods) {
-      try {
-        Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
-        x.setAccessible(true);
-        System.out.println(x.toString());
-        x.invoke(this, entity, "player");
-      }
-      catch (Exception e){        System.out.println(e);
-
-        System.out.println("afasdasdasdzasdfbgds");
-        return;
-      }
+    public Coin(int objectWidth, int objectHeight, double initialX, double initialY) {
+        super(objectWidth, objectHeight, initialX, initialY);
+        this.setHitpoints(COIN_HEALTH);
+        setWidth(COIN_SIZE);
+        setHeight(COIN_SIZE);
+        setId("coin");
     }
-    if(this.getHitpoints() == 0){
 
-      System.out.println("tedsad123");
+    @Override
+    public void leftCollideable(Entity entity) {
+        GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
+        Collection<String> methods = reader.getMethods("left");
+        System.out.println("tedsad");
+        for (String k : methods) {
+            try {
+                Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
+                x.setAccessible(true);
+                System.out.println(x.toString());
+                x.invoke(this, entity, "player");
+            } catch (Exception e) {
+                throw new MethodNotFound("Could not find reflected method from property file");
+            }
+        }
+        if (this.getHitpoints() == 0) {
+
+            System.out.println("tedsad123");
+        }
     }
-  }
 
 
-  @Override
-  public void rightCollideable(Entity entity) {
-    GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
-    Collection<String> methods = reader.getMethods("right");
-    System.out.println("tedsad");
-    for (String k: methods) {
-      try {
-        Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
-        System.out.println(x.toString());
-        x.setAccessible(true);
-        x.invoke(this, entity, "player");
-        System.out.println("helloooo");
-      }
-      catch (Exception e){     e.printStackTrace();
-        return;
-      }
+    @Override
+    public void rightCollideable(Entity entity) {
+        GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
+        Collection<String> methods = reader.getMethods("right");
+        System.out.println("tedsad");
+        for (String k : methods) {
+            try {
+                Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
+                System.out.println(x.toString());
+                x.setAccessible(true);
+                x.invoke(this, entity, "player");
+                System.out.println("helloooo");
+            } catch (Exception e) {
+                throw new MethodNotFound("Could not find reflected method from property file");
+            }
+        }
+        if (this.getHitpoints() == 0) {
+
+            System.out.println("tedsad123");
+        }
     }
-    if(this.getHitpoints() == 0){
 
-      System.out.println("tedsad123");
+    @Override
+    public void bottomCollideable(Entity entity) {
+        GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
+        Collection<String> methods = reader.getMethods("bottom");
+        System.out.println("tedsad");
+        for (String k : methods) {
+            try {
+                Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
+                System.out.println(x.toString());
+                x.setAccessible(true);
+                x.invoke(this, entity, "player");
+            } catch (Exception e) {
+                throw new MethodNotFound("Could not find reflected method from property file");
+            }
+        }
+        if (this.getHitpoints() == 0) {
+
+            System.out.println("tedsad123");
+        }
     }
-  }
 
-  @Override
-  public void bottomCollideable(Entity entity) {
-    GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
-    Collection<String> methods = reader.getMethods("bottom");
-    System.out.println("tedsad");
-    for (String k: methods) {
-      try {
-        Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
-        System.out.println(x.toString());
-        x.setAccessible(true);
-        x.invoke(this, entity, "player");
-      }
-      catch (Exception e){
-        return;
-      }
+    @Override
+    public void topCollideable(Entity entity) {
+        GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
+        Collection<String> methods = reader.getMethods("top");
+        Collection<String> parameters = reader.getParameters("top");
+        System.out.println("tedsad");
+        for (String k : methods) {
+            try {
+                Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
+                System.out.println(x.toString());
+                x.setAccessible(true);
+                x.invoke(this, entity, "player");
+            } catch (Exception e) {
+                throw new MethodNotFound("Could not find reflected method from property file");
+            }
+        }
+        if (this.getHitpoints() == 0) {
+
+            System.out.println("tedsad123");
+        }
     }
-    if(this.getHitpoints() == 0){
 
-      System.out.println("tedsad123");
-    }
-  }
-
-  @Override
-  public void topCollideable(Entity entity) {
-    GamePropertyFileReader reader = new GamePropertyFileReader("Coin");
-    Collection<String> methods = reader.getMethods("top");
-    Collection<String> parameters = reader.getParameters("top");
-    System.out.println("tedsad");
-    for (String k: methods) {
-      try {
-        Method x = this.getClass().getSuperclass().getDeclaredMethod(k, Entity.class, String.class);
-        System.out.println(x.toString());
-        x.setAccessible(true);
-        x.invoke(this, entity, "player");
-      }
-      catch (Exception e){
-        return;
-      }
-    }
-    if(this.getHitpoints() == 0){
-
-      System.out.println("tedsad123");
-    }
-  }
-
-  }
+}
 
 
