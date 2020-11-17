b09af6b0dedc30c956a9785a37640177be1ae008
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-16 20:35:44 -0500

    writing tests for animation class

---
+++ test/ooga/view/AnimationTest.java
@@ -0,0 +1,41 @@
+package ooga.view;
+
+import ooga.loader.AnimationBrain;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimationTest {
+AnimationBrain information = new AnimationBrain("MarioGame");
+    @Test
+    void setX() {
+    }
+
+    @Test
+    void setY() {
+    }
+
+    @Test
+    void swapDirection() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void setScaleX() {
+    }
+
+    @Test
+    void setScaleY() {
+    }
+
+    @Test
+    void interpolate() {
+    }
+
+    @Test
+    void getImage() {
+    }
+}
