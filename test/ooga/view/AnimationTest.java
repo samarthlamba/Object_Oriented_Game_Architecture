c031de17050789794c14b964f224358f51396171
Samarth Lamba <samarth.lamba@duke.edu>
2020-11-16 20:41:59 -0500

    added some animation tests

--- test/ooga/view/AnimationTest.java
+++ test/ooga/view/AnimationTest.java
@@ -6,21 +6,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimationTest {
-AnimationBrain information = new AnimationBrain("MarioGame");
+AnimationBrain animationBrain = new AnimationBrain("MarioGame");
+
+    int length = animationBrain.getLengthMap().get(AnimationState.WALK);
+    int pos = animationBrain.getPositionOfFirstAnimationMap().get(AnimationState.WALK);
+    int duration = animationBrain.getDurationMap().get(AnimationState.WALK);
+    Animation animation = new Animation(animationBrain.getImage(),50.0,50.0,animationBrain.getxWhiteSpaceConstant(),animationBrain.getyWhiteSpaceConstant(),length,pos, animationBrain.getFramesPerRow(), animationBrain.getWidthActual(), animationBrain.getHeightActual(),duration);
+
     @Test
     void setX() {
+        animation.setX(21);
+        assertEquals(21, animation.getImage().getX());
+        animation.setX(31);
+        assertEquals(31, animation.getImage().getX());
     }
 
     @Test
     void setY() {
+        animation.setY(25);
+        assertEquals(25, animation.getImage().getX());
+        animation.setY(32);
+        assertEquals(32, animation.getImage().getX());
     }
 
     @Test
     void swapDirection() {
+        animation.swapDirection();
+        assertEquals(-1, animation.getImage().getScaleX());
     }
 
     @Test
     void scale() {
+        animation.scale(30, 30);
+        assertEquals(30, animation.getImage().getFitHeight());
+        assertEquals(30, animation.getImage().getFitWidth());
     }
 
     @Test
